a98af2ab88aba11fbe5cbe92f5ac9f9dfdd8f5ac
christine <rickyshie0724@gmail.com>
2023-09-21 11:48:35 +0900

    Arrange weekly specials' title and online menu button's layout for mobile devices

--- src/components/Specials/Specials.js
+++ src/components/Specials/Specials.js
@@ -1,10 +1,30 @@
 import React from "react";
+import Button from "../Button/Button";
 import './Specials.css'
 
+
 function Specials(){
-    return (<section className="weekly-specials">
-        <h1>Hello</h1>
-    </section>)
+    function handleButtonClick(){
+        console.log('Button has been clicked');
+    }
+    return (
+    <section className="weekly-specials">
+        <div className="weekly-specials-item-title">
+            <h1 className="weekly-specials-title">
+                This week's specials!
+            </h1>
+        </div>
+        <div className="weekly-specials-item-carousel">
+            {/* <h1 className="weekly-specials-title">
+                This week's specials!
+            </h1> */}
+            Create a carousel component here
+        </div>
+        <div className="weekly-specials-item-button">
+        <Button className="online-menu-button" label="Online Menu" onClick={handleButtonClick}></Button>
+        </div>
+    </section>
+    )
 }
 
 export default Specials;
